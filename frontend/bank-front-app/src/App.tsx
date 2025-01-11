63b2c28635856b081a60ada7efafd0e5ffade928
Egor <alyanov.egor2018@yandex.ru>
2025-01-11 15:26:53 +0300

    tauri ready

--- frontend/bank-front-app/src/App.tsx
+++ frontend/bank-front-app/src/App.tsx
@@ -4,17 +4,30 @@ import HomePage from "./pages/HomePage";
 import OfferPage from "./pages/OfferPage";
 import OffersPage from "./pages/OffersPage";
 import { ROUTES } from "./Routes";
+import { invoke } from "@tauri-apps/api/core";
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <BrowserRouter basename='/Bank-Offer-Frontend'>
-      <Routes>
-        <Route path={ROUTES.HOME} index element={<HomePage />} />
-        <Route path={ROUTES.OFFERS} element={<OffersPage />} />
-        <Route path={`${ROUTES.OFFERS}/:id`} element={<OfferPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    useEffect(()=>{
+        invoke('tauri', {cmd:'create'})
+          .then(() =>{console.log("Tauri launched")})
+          .catch(() =>{console.log("Tauri not launched")})
+        return () =>{
+          invoke('tauri', {cmd:'close'})
+            .then(() =>{console.log("Tauri launched")})
+            .catch(() =>{console.log("Tauri not launched")})
+        }
+    }, [])
+
+    return (
+        <BrowserRouter basename='/Bank-Offer-Frontend'>
+        <Routes>
+            <Route path={ROUTES.HOME} index element={<HomePage />} />
+            <Route path={ROUTES.OFFERS} element={<OffersPage />} />
+            <Route path={`${ROUTES.OFFERS}/:id`} element={<OfferPage />} />
+        </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
